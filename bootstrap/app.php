730afd92680dab56c7d9b5c71aa71e6fee91bc0a
InoussaLouqmane <inoussalouqmane@gmail.com>
2025-06-24 02:04:35 +0100

    Fixed redirection error on unauthorized request

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,7 +5,9 @@ use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\Routing\Exception\RouteNotFoundException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -18,4 +20,47 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
+
+        $exceptions->shouldRenderJsonWhen(function (Request $request, Throwable $e) {
+
+            if ($request->is('api/*')) {
+                return true; // Always render JSON for 'api/*' routes
+            }
+            return $request->expectsJson();
+        });
+
+
+        $exceptions->render(function (AuthenticationException $e, Request $request) {
+            return response()->json([
+                'message' => 'Vous n\'êtes pas authentifié(e) ou token invalide.',
+            ], 401);
+        });
+
+        $exceptions->render(function (AuthorizationException $e, Request $request) {
+            return response()->json([
+                'message' => 'Vous n\'êtes pas autorisé(e) à effectuer cette action.',
+            ], 403);
+        });
+
+        $exceptions->render(function (RouteNotFoundException $e, Request $request) {
+            // Check if the exception message contains anything related to login
+            if (str_contains($e->getMessage(), 'login')) {
+                return response()->json(
+                    [
+                        'message' => 'Vous n\'êtes pas autorisé | Token Invalide'
+                    ],
+                    401
+                );
+            }
+
+            return response()->json(
+                [
+                    'success' => false,
+                    'message' => 'Endpoint not found.'
+                ],
+                404
+            );
+        });
+
+
     })->create();
